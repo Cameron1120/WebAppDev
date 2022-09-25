f895e844cc13abc2f9df2c4bf8fdd9723a4a400c
Ubuntu <ubuntu@ip-172-31-35-43.us-east-2.compute.internal>
2022-09-25 21:57:53 +0000

    newsfeed

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -22,9 +22,13 @@ module.exports = {
                 },
                 {
                test: /\.css$/,
-               use: ['style-loader', 'css-loader']
-           }
-       ]
+               use: ['style-loader', 'css-loader'],
+		},
+		{
+   		test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+     		 loader: 'file-loader' 
+     		 }
+	    ]
    },
    devServer: {
 
